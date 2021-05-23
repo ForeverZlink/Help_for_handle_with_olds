9511d732ba225fc2e8d3bda6a9f104419af6f6c8
St;ZlinkG <carloscunha080@gmail.com>
2021-05-22 21:20:15 -0300

    Criei uma tabela que mostra os idosos e os não idosos. Criei uma documentação para minhas funções e adicionei alguns comentários em meu código.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from modulo import separa_idosos, fila_prioridade_por_idosos
+from modulo import separa_idosos, fila_prioridade_por_idosos, mostra_tabela_idosos
 
 
 pessoas_cadastradas = {
@@ -9,7 +9,7 @@ pessoas_cadastradas = {
 while True:
     name = str(input('Digite o nome:')).strip()
     age  = int(input('Agora a idade:'))
-    opcao = str(input('Quer continuar?s ou n')).strip().upper()
+    opcao = str(input('Quer continuar?S/N')).strip().upper()
     pessoas_cadastradas['nome'].append(name)
     pessoas_cadastradas['idade'].append(age)
     if opcao == 'N':
@@ -20,10 +20,9 @@ pessoas_separadas = separa_idosos(pessoas_cadastradas,60)
 
 fila_ordenada= fila_prioridade_por_idosos(pessoas_separadas,2)
 
+mostra_tabela_idosos(pessoas_separadas)
 
 
-
-print(fila_ordenada)
 
 
 
--- modulo.py
+++ modulo.py
@@ -1,8 +1,17 @@
-def separa_idosos(pessoas,idade_para_ser_idoso):
+def separa_idosos(pessoas:dict,idade_para_ser_idoso:int)->dict:
+    """
+    Separa os idosos em uma lista que está dentro de um dicionário.
+    Parametro pessoas: possui as pessoas para serem separadas 
+    Parametro idadae : é com quantos anos alguém é idoso.
+    
+    """
     pessoas_separadas = {
         'maiores_60':[],'menores_60':[]
     }
+
+    #varre o dicionario
     for nome, idade in zip(pessoas['nome'],pessoas['idade']):
+        
         if idade>=idade_para_ser_idoso:
             pessoas_separadas['maiores_60'].append([nome,idade])
         else:
@@ -11,23 +20,69 @@ def separa_idosos(pessoas,idade_para_ser_idoso):
     return pessoas_separadas
 
 
-def fila_prioridade_por_idosos(pessoas, condição_de_ordenação_fila):
+def fila_prioridade_por_idosos(pessoas:dict, condição_de_ordenação_fila:int)->list:
+    """
+    gera uma fila de prioridade a partir da idade 
+    Parametro pessoas: é as pessoas que serão ordenadas em fila
+    Parametro condição_de_ordenação_fila: é o critério para separar a fila. EXemplo: A cada dois idosos, um nao idoso é atendido.
+                                         Esse parametro é que decide a quantidade de idosos 
+
+    """
+    
+    #recebe uma copia dos valores das listas  que estao dentro do dicionario pessoas
+    #pois se isso nao for feito, uma ligação entre lista é criada no programa principal e o 
+    #a variavel do programa principal fica ligada as listas dessas função
+    pessoas = {
+        'maiores_60':pessoas['maiores_60'].copy(),
+        'menores_60':pessoas['menores_60'].copy()
+        }
+
     fila_prioridade=[]
     quantidade_de_pessoas = len(pessoas['maiores_60'])+len(pessoas['menores_60'])
+    
     #a cada x idosos, um nao idoso é atendido.
     contador=0
+
+    #Esse loop vai acontecer o mesmo numero de vezes que 
+    #existas pessoas, assim nao terei problems of index
     for people_cont in range (0,quantidade_de_pessoas):
-        
+
+        # se nao ha maiores de 60, entao eu posso adicionar.
         if len(pessoas['maiores_60'])==0 or contador ==condição_de_ordenação_fila:
                 contador=0
                 fila_prioridade.append(pessoas['menores_60'][0])
+                #apaga o primero valor
                 pessoas['menores_60'].pop(0)
 
         
         else:
+            #se a lista estiver vazia 
             if pessoas['maiores_60']!=[]:
                 fila_prioridade.append(pessoas['maiores_60'][0])
+                #apago o primeiro valor da lista
                 pessoas['maiores_60'].pop(0)
         contador+=1
-        
-    print(fila_prioridade)
+
+    return fila_prioridade
+    
+
+def mostra_tabela_idosos(pessoas:dict):
+    """
+    Mostra uma tabela com idosos e nao idosos.
+    Só será mostrado as lista que possuir valores
+    
+    """
+    #varre  o dicionario 
+    for nome_categoria, value in pessoas.items():
+        ##caso a lista esteja vazia, o programa vai para a próxima iteração
+        if value == []:
+            continue
+        print('-+'*50)
+        print(f'{nome_categoria:^30}'.upper())
+        print(f'{"Nome":<40} {"idade"}')
+
+
+        ##varre a lista 
+        for pessoa in value:
+            print(f'{pessoa[0]:<42}{pessoa[1]}')
+        print('-+'*50)
